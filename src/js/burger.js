2a23468646939fd3e83ef7383f2e577859b23308
Bogdan Tkachenko <61245273+Korsavabi@users.noreply.github.com>
2020-08-28 17:01:22 +0300

    правки в бургере

--- src/js/burger.js
+++ src/js/burger.js
@@ -3,10 +3,10 @@ window.onload = function (){
     openMenu.onclick = function () {
         if (menuStyle.display == "none"){
             menu.classList.add("active");
-            this.innerHTML = '<svg class="button-burger-close-svg"><use href="images/img-project/svg/sprite.svg#icon-menu"></use></svg>';
+            this.innerHTML = '<svg class="header-navbars-burger-close-svg"><use href="sprite.svg#icon-close"></use></svg>';
         } else {
             menu.classList.remove("active");
-            this.innerHTML = '<svg class="button-burger-open-svg"><use href="images/img-project/svg/sprite.svg#icon-close"></use></svg>';
+            this.innerHTML = '<svg class="header-navbars-burger-open-svg"><use href="sprite.svg#icon-menu"></use></svg>';
         }
     }
 }
