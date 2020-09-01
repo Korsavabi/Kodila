72aee2b07f395c74ce1b2ae737c4855209e64856
Bogdan Tkachenko <61245273+Korsavabi@users.noreply.github.com>
2020-09-01 10:42:20 +0300

    карусель

---
+++ src/js/carousel.js
@@ -0,0 +1,37 @@
+
+jQuery(document).ready(function($) {
+    
+    let outs = document.querySelectorAll('.owl-out');
+
+    for (let out of outs) {
+
+        let owl = out.querySelector('.owl-carousel');
+        let btns = out.querySelectorAll(':scope .thumb');
+
+        let owlJq = $(owl);
+        owlJq.owlCarousel({
+            loop: true,
+            margin: 10,
+            nav:true,
+            dots: false,
+            responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+    }
+        });
+
+        for (let btn of btns) {
+            btn.addEventListener('click', function () {
+                owlJq.trigger('to.owl.carousel', [$(this).index()]);
+            })
+        }
+    };
+
+});
