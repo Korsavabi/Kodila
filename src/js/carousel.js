d151a6a5412c7759a9f04109849be287a23496b6
Korsavabi <61245273+Korsavabi@users.noreply.github.com>
2020-09-02 03:27:36 +0300

    pravki

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -10,9 +10,16 @@ jQuery(document).ready(function($) {
 
         let owlJq = $(owl);
         owlJq.owlCarousel({
+            Type: Boolean,
+            Default: false,
             loop: true,
             margin: 10,
-            nav:false,
+            responsiveClass: true,
+            nav:true,
+            navText: [
+                '<span aria-label="' + 'Previous' + '"><img class="carusel-prev-icon" src="./iamges/svg/Arrow-1.png" alt=""></span>',
+                '<span aria-label="' + 'Next' + '"><svg class="arrow-right" aria-hidden="true"><use href="./images/svg/arrow.svg#arrow-right"></use></svg></span>'
+            ],
             dots: false,
             responsive: {
             0: {
